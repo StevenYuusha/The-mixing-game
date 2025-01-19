a326d5be85b985730e8e493e6d47ab9123503ef8
StevenYuusha <hl38822@gmail.com>
2025-01-19 06:12:18 -0500

    bottle and walls

---
+++ Assets/PourEffect-master/PourEffect-master/Assets/Scripts/PourDetector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject StreamPrefab = null;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngel() < pourThreshold;
+
+        if(isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+            if (isPouring)
+            {
+                StartPour();
+            } else 
+            {
+                EndPour();
+            }
+        }
+
+    }
+
+    private void StartPour()
+    {
+        print("start");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+        print("end");
+        currentStream.End();
+        currentStream = null;
+    }
+
+    private float CalculatePourAngel()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(StreamPrefab, origin.position, Quaternion.identity, transform);
+
+        return streamObject.GetComponent<Stream>();
+
+    }
+}
